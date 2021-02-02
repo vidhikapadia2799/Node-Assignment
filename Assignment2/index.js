cd1f132ac6e3fa9f57da1e29ae1b4a3b0fc59c62
Vidhi Kapadia <vidhikap27@gmail.com>
2021-02-02 21:23:46 +0530

    commiting Assignment3 and Assignment4

--- Assignment2/index.js
+++ Assignment2/index.js
@@ -2,6 +2,28 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const db = require('./queries')
 
+var multer  = require('multer');
+var storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, './uploads/images');
+    },
+    filename: (req, file, cb) => {
+      //console.log(file);
+      var filetype = '';
+      if(file.mimetype === 'image/gif') {
+        filetype = 'gif';
+      }
+      if(file.mimetype === 'image/png') {
+        filetype = 'png';
+      }
+      if(file.mimetype === 'image/jpeg') {
+        filetype = 'jpg';
+      }
+      cb(null, 'image-' + Date.now() + '.' + filetype);
+    }
+});
+var upload = multer({ storage: storage });
+
 const app = express()
 const port = 3000
 
@@ -19,6 +41,21 @@ app.get('/', (request, response) => {
 app.get('/car', db.getCars)
 app.get('/car/:id', db.getCarsById)
 app.post('/car', db.createCar)
+app.delete('/car/:id', db.deleteCar)
+app.post('/upload/:id',upload.single('CarImage'),function(req,res,next){
+  
+  if (!req.file) {
+    const error = new Error('Please upload a file')
+    error.httpStatusCode = 400
+    return next(error)
+  }
+  else{
+      db.insertCarImage(req,res)
+  }
+  
+})
+
+app.use('*/images',express.static('uploads/images'));
 
 app.listen(port, () => {
   console.log(`App running on port ${port}.`)
--- Assignment2/queries.js
+++ Assignment2/queries.js
@@ -8,7 +8,7 @@ const pool = new Pool({
 })
 
 const getCars = (request, response) => {
-  pool.query('SELECT car.id,car.name,model.modelname,make.makename FROM car INNER JOIN model on car.id=model.modelid INNER JOIN make On model.modelid=make.makeid', (error, results) => {
+  pool.query('SELECT car.carid,car.name,model.modelname,make.makename FROM car INNER JOIN model on car.carid=model.modelid INNER JOIN make On model.modelid=make.makeid', (error, results) => {
     if (error) {
       throw error
     }
@@ -20,7 +20,7 @@ const getCars = (request, response) => {
 const getCarsById = (request, response) => {
   const id = parseInt(request.params.id)
 
-  pool.query('SELECT car.id,car.name,model.modelname,make.makename FROM car INNER JOIN model on car.id=model.modelid INNER JOIN make On model.modelid=make.makeid WHERE car.id = $1', [id], (error, results) => {
+  pool.query('SELECT car.carid,car.name,model.modelname,make.makename FROM car INNER JOIN model on car.carid=model.modelid INNER JOIN make On model.modelid=make.makeid WHERE car.carid = $1', [id], (error, results) => {
 
     if (error) {
       throw error
@@ -28,39 +28,90 @@ const getCarsById = (request, response) => {
     response.status(200).json(results.rows)
   })
 }
-const createCar = (request, response) => {
-  const carName = request.body.name;
-  console.log(request.body)
 
-  if (checkIfCarAlreadyExists(carName)) {
-    response.json("Car Already Exist");
-    return;
+const createCar = async (request,response) => {
+  let carName = request.body.name;
+  let makeName = request.body.makename;
+  let modelName = request.body.modelname;
+
+  console.log(carName);
+
+  const oldCar = await pool.query("Select name from car where name = $1",[carName]);
+  if(oldCar.rowCount >= 1){
+    response.send("Car already exist");
   }
-  else {
-    response.json("Car does not exist");
-    return;
-  }
- 
-  function checkIfCarAlreadyExists(carName) {
-    console.log(carName)
-    pool.query('Select * FROM car WHERE name = $1', [carName], (error, result) => {
-      if (error) {
-        response.status(500).json("Internal Server Error");
-        throw error;
-      }
-      else if (result.rowCount > 0)
-        return true;
-      else
-        return false;
+  else{
+     //Checking about makename exists or not
+    const oldMakeId = await pool.query("Select makeid,makename from make where makename = $1",[makeName]);
+    if(oldMakeId.rowCount >= 1){
+      newMakeId = oldMakeId.rows[0].makeid;
+      console.log(newMakeId);
     }
-    )}
+    else{
+          const result1 = await pool.query("Insert into make(makename) values($1) RETURNING makeid",[makeName]);
+          if(result1.rowCount >= 1){
+            newMakeId = await pool.query("Select makeid,makename from make where makename = $1",[makeName]);
+          }
+          if(newMakeId.rowCount >= 1){
+            newMakeId = newMakeId.rows[0].makeid;
+            console.log(newMakeId);
+          }
+      }
+      //Checking about modelname exists or not
+      const oldModelId = await pool.query("Select modelid,modelname from model where modelname = $1",[modelName]);
+        if(oldModelId.rowCount >= 1){
+        newModelId = oldModelId.rows[0].modelid;
+        console.log(newModelId);
+      }
+      else{
+            const result2 = await pool.query("Insert into model(modelname) values($1) RETURNING modelid",[modelName]);
+            if(result2.rowCount >= 1){
+              newModelId = await pool.query("Select modelid,modelname from model where modelname = $1",[modelName]);
+            }
+            if(newModelId.rowCount >= 1){
+              newModelId = newModelId.rows[0].modelid;
+              console.log(newModelId);
+            }
+      }
+
+      await pool.query("Insert into car(name,makeid,modelid) values($1,$2,$3) RETURNING carid",[carName,newMakeId,newModelId],(error,result) =>{
+        if(error)
+          throw error;
+        response.status(201).send(`Successfully added new Car`)
+      } )
+  }
 }
 
+const deleteCar = (request, response) => {
+  const id = parseInt(request.params.id)
+
+  pool.query('DELETE FROM car WHERE carid = $1', [id], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).send(`Car deleted with ID: ${id}`)
+  })
+}
+
+
+const insertCarImage = (request,response) => {
+    let id = parseInt(request.body.id);
+    let carId = parseInt(request.params.id);
+    let fileName = request.file.filename;
+
+    pool.query("Insert into carimage(filename,carid) values($1,$2)",[fileName,carId],(error,result) => {
+     
+      if(error){
+        throw error;
+      }
+      response.status(201).json(`Image Uploaded Succesfully`)
+    })
+}
 
 module.exports = {
   getCars,
   getCarsById,
-  createCar
-  //   updateUser,
-  //   deleteUser,
+  createCar,
+  deleteCar,
+  insertCarImage
 }
